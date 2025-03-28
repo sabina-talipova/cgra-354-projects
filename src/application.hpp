3235ac4a5497f0a7cb91db2bd34827c6bed0a00f
unknown <tasabina@yandex.ru>
2025-03-29 11:09:23 +1300

    Assignment_2: Initial commit

---
+++ src/application.hpp
@@ -0,0 +1,57 @@
+
+#pragma once
+
+// glm
+#include <glm/glm.hpp>
+
+// project
+#include "opengl.hpp"
+#include "basic_model.hpp"
+
+
+// Main application class
+//
+class Application {
+private:
+	// window
+	glm::vec2 m_windowsize;
+	GLFWwindow *m_window;
+
+	// Assignment_2
+	bool m_mouse_click = false;
+	glm::vec2 m_mouse_pos;
+	float m_yaw = -0.9;
+	float m_pitch = 0.9;
+
+	// oribital camera
+	float m_distance = 20.0;
+
+	// drawing flags
+	bool m_show_axis = false;
+	bool m_show_grid = false;
+	bool m_showWireframe = false;
+
+	// basic model
+	// contains a shader, a model transform
+	// a mesh, and other model information (color etc.)
+	basic_model m_model;
+
+public:
+	// setup
+	Application(GLFWwindow *);
+
+	// disable copy constructors (for safety)
+	Application(const Application&) = delete;
+	Application& operator=(const Application&) = delete;
+
+	// rendering callbacks (every frame)
+	void render();
+	void renderGUI();
+
+	// input callbacks
+	void cursorPosCallback(double xpos, double ypos);
+	void mouseButtonCallback(int button, int action, int mods);
+	void scrollCallback(double xoffset, double yoffset);
+	void keyCallback(int key, int scancode, int action, int mods);
+	void charCallback(unsigned int c);
+};
