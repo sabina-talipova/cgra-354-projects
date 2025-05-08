86536f998ef8ef68bccaead2e12b54b3f954942c
unknown <tasabina@yandex.ru>
2025-05-08 21:02:14 +1200

    Initial commit

---
+++ src/boid.cpp
@@ -0,0 +1,61 @@
+
+// glm
+#include <glm/gtc/random.hpp>
+
+// project
+#include "boid.hpp"
+#include "scene.hpp"
+#include "cgra/cgra_mesh.hpp"
+
+
+using namespace glm;
+using namespace std;
+
+
+vec3 Boid::color() const {
+	return vec3(0, 1, 0);
+}
+
+
+void Boid::calculateForces(Scene *scene) {
+	//-------------------------------------------------------------
+	// [Assignment 3] :
+	// Calculate the forces affecting the boid and update the
+	// acceleration (assuming mass = 1).
+	// Do NOT update velocity or position in this function.
+	// Core : 
+	//  - Cohesion
+	//  - Alignment
+	//  - Avoidance
+	//  - Soft Bound (optional)
+	// Completion : 
+	//  - Cohesion and Alignment with only boids in the same flock
+	//  - Predator Avoidance (boids only)
+	//  - Predator Chase (predator only)
+	// Challenge : 
+	//  - Obstacle avoidance
+	//-------------------------------------------------------------
+
+	// YOUR CODE GOES HERE
+	// ...
+
+}
+
+
+void Boid::update(float timestep, Scene *scene) {
+	//-------------------------------------------------------------
+	// [Assignment 3] :
+	// Integrate the velocity of the boid using the timestep.
+	// Update the position of the boid using the new velocity.
+	// Take into account the bounds of the scene which may
+	// require you to change the velocity (if bouncing) or
+	// change the position (if wrapping).
+	//-------------------------------------------------------------
+
+	// YOUR CODE GOES HERE
+	// ...
+
+
+	m_position += m_velocity * timestep;
+
+}
